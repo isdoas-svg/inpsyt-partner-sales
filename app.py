1fa7157407f8f63d105807cc9c79da49f55bc7e1
isdoas-svg <isdoas@gmail.com>
2026-09-16 18:35:40 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -919,9 +919,9 @@ def main_dashboard():
                 <style>
                 div[data-testid="stRadio"] > label { font-weight: bold; margin-bottom: 8px; }
                 div[data-testid="stRadio"] div[role="radiogroup"] > label { 
-                    padding-top: 12px; !important;
-                    padding-bottom: 12px; !important;
-                    margin-bottom: 18px; !important;
+                    padding-top: 12px !important;
+                    padding-bottom: 12px !important;
+                    margin-bottom: 18px !important;
                 }
                 </style>
             """,
